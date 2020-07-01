e3c9bd9085b75c1eb7d528d96b3b20197ac8b211
Alexandre GAZAGNES <alexandre@coolparentsmakehappykids.com>
2020-07-01 11:50:32 +0200

    add all

---
+++ cureCuma/services/back/run.py
@@ -0,0 +1,22 @@
+# !/usr/bin/env python3
+# coding: utf-8
+
+import sys
+import time
+
+from src import create_app
+from params import GeneralParams as Params
+
+
+app = create_app()
+
+
+def main():
+    """main funct"""
+
+    app = create_app()
+    app.run(port=Params.port, host=Params.host)
+
+
+if __name__ == "__main__":
+    main()
